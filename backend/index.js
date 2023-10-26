a75ccaa22162927067c811ff4f10bde1428ad1d5
= <=>
2023-10-26 11:38:13 -0400

    sets up basic middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
+const dotenv = require("dotenv");
 const express = require('express');
+
 const app = express(); // create express app
+dotenv.config(); // initialize dotenv
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method); // log the request path and method
+    next(); // call next to move on to the next middleware
+});
 
 // routes
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
 
-app.listen(4000, () => console.log('Listening on port 4000')); // listen for requests
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`)); // listen for requests
