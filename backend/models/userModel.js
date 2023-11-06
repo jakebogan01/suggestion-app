02e3e0980654081a6b500e775f4ef1037b58accc
Jake Bogan <jake@Jakes-MacBook-Air.local>
2023-11-06 12:26:25 -0500

    adds user validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require( "mongoose" );
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
@@ -18,30 +19,24 @@ const UserSchema = new Schema(
 );
 
 UserSchema.statics.register = async function (email, password) {
-    // // validation
-    // if (!email || !password) {
-    //     throw new Error("All fields must be provided");
-    // }
-    // // check if email is valid
-    // if (!validator.isEmail(email)) {
-    //     throw new Error("Email is invalid");
-    // }
-    // // check if password is strong enough
-    // if (!validator.isStrongPassword(password)) {
-    //     throw new Error("Password is not strong enough");
-    // }
+    if (!email || !password) {
+        throw new Error("All fields must be provided");
+    }
+    if (!validator.isEmail(email)) {
+        throw new Error("Email is invalid");
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw new Error("Password is not strong enough");
+    }
 
-    // Check if email exists
     const exists = await this.findOne({ email });
     if (exists) {
         throw new Error("Email already in use");
     }
 
-    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
 
-    // Create a new user
     const user = await this.create({ email, password: hash });
 
     return user;
