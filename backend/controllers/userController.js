f991edc22a852b38b59c55c47cadb81644ccdff8
Jake Bogan <jake@Jakes-MacBook-Air.local>
2023-11-07 14:49:37 -0500

    adds more functions to user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require( "../models/userModel" );
 const mongoose = require( "mongoose" );
 const jwt = require("jsonwebtoken");
+const Suggestion = require("../models/suggestionModel");
 
 // create token
 const createToken = (_id) => {
@@ -9,6 +10,7 @@ const createToken = (_id) => {
     });
 };
 
+// LOGIN user
 const login = async (req, res) => {
     const { email, password } = req.body;
 
@@ -21,6 +23,7 @@ const login = async (req, res) => {
     }
 }
 
+// REGISTER user
 const register = async (req, res) => {
     const { email, password } = req.body;
 
@@ -33,4 +36,80 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = { login, register };
+// GET all users
+const getUsers = async ( req, res ) => {
+     const users = await User.find().select( "email" ).sort({ createdAt: -1 });
+
+     try {
+          res.status( 200 ).json( users );
+     } catch ( error ) {
+          res.status( 400 ).json({ error });
+     }
+};
+
+// GET user
+const getUser = async ( req, res ) => {
+    const { id } = req.params;
+
+    if ( !mongoose.Types.ObjectId.isValid( id ) ) {
+        return res.status( 404 ).json({ error: "No such user" });
+    }
+
+    const user = await User.find({ _id: id });
+
+    if ( !user ) {
+        return res.status( 404 ).json({ error: "No such user" });
+    }
+
+    try {
+        res.status( 200 ).json( user );
+    } catch ( error ) {
+        res.status( 400 ).json({ error });
+    }
+};
+
+// DELETE user
+const deleteUser = async ( req, res ) => {
+     const { id } = req.params;
+
+     if ( !mongoose.Types.ObjectId.isValid( id ) ) {
+          return res.status( 404 ).json({ error: "No such user" });
+     }
+
+     const user = await User.findOneAndDelete({ _id: id });
+
+     if ( !user ) {
+          return res.status( 404 ).json({ error: "No such user" });
+     }
+
+     try {
+          res.status( 200 ).json({ message: "User deleted successfully" });
+     } catch ( error ) {
+          res.status( 400 ).json({ error });
+     }
+};
+
+// UPDATE user
+const updateUser = async ( req, res ) => {
+     const { id } = req.params;
+
+     if ( !mongoose.Types.ObjectId.isValid( id ) ) {
+          return res.status( 404 ).json({ error: "No such user" });
+     }
+
+     const user = await User.findOneAndUpdate(
+         { _id: id }, { ...req.body }
+     );
+
+     if ( !user ) {
+          return res.status( 404 ).json({ error: "No such user" });
+     }
+
+     try {
+          res.status( 200 ).json({ message: "User updated successfully" });
+     } catch ( error ) {
+          res.status( 400 ).json({ error });
+     }
+};
+
+module.exports = { login, register, getUsers, getUser, deleteUser, updateUser };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { login, register } = require("../controllers/userController");
+const { login, register, getUsers, getUser, deleteUser, updateUser } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/login", login); // LOGIN
 router.post("/register", register); // REGISTER
+router.get( "/", getUsers ); // GET all users
+router.get( "/:id", getUser); // GET user
+router.delete( "/:id", deleteUser); // DELETE user
+router.patch( "/:id", updateUser); // UPDATE user
 
 module.exports = router;
